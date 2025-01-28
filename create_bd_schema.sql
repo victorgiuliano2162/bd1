89ec73271d9392f497a84dc80cba7ca39e13d571
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-01-28 13:39:38 -0300

    create bd

---
+++ create_bd_schema.sql
@@ -0,0 +1,7 @@
+/*
+Crie um banco de dados, depois crie a tabela abaixo nele, insira os valores apresentados
+e em seguida escreva as consultas solicitadas abaixo.
+OBS: Os valores em branco devem ser nulos no banco de dados.
+*/
+
+CREATE SChEMA exercicios_bd;
