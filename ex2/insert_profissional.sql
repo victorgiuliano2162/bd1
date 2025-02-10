d16a15798599ae005168557f912d2b9efef2f621
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-10 17:15:19 -0300

    insert into profissional

---
+++ ex2/insert_profissional.sql
@@ -0,0 +1,7 @@
+INSERT INTO exercicios_bd2.profissional 
+    (cpf, nome, endereco, data_nascimento, profissao)
+    values 
+    ("190.243.345-34", "Jacomorac Pacini", "Rua Centerback 23", "1988-02-01", "Marceneiro"),
+    ("343.566.235-12", "Van Borad", "Rua Attack Midfield 20", "1985-05-05", "Soldador"),
+    ("234.234.556-12", "Gellazca Macovshire", "Rua Captain 2013 106", "2000-04-09", "Batedor de Massa"),
+    ("944.323.120-10", "Jaric Ruskin", "Rua Wingback 36", "1987-04-3", "Soldador")
