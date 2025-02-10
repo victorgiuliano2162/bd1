86c0a626f74cd619fa4d3ed8e85c6f3341e9be42
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-10 17:15:12 -0300

    insert into empresa

---
+++ ex2/insert_empresa.sql
@@ -0,0 +1,6 @@
+INSERT INTO exercicios_bd2.empresa 
+    (cnpj, nome_fantasia, endereco)
+    values 
+    ("02.847.655/0001-73", "Construtora Ou Minha Casa ou Minha Vida", "Rua Seinfeld, 1989"),
+    ("10.243.355/0010-12", "Muito Remendado e Velho Construções", "Rua Fullhouse, 1987"),
+    ("56.924.320/0007-64", "Apêlar Imóveis", "Rua Fresh Prince of Bel-Air, 1990")
