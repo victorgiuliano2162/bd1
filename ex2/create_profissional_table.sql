c127e47d8b28bfe61dbd4f5686352a0518ad919d
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-10 17:14:54 -0300

    table profissional

---
+++ ex2/create_profissional_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE exercicios_bd2.profissional (
+    cpf VARCHAR(20) PRIMARY KEY,
+    nome VARCHAR(128),
+    endereco VARCHAR(256),
+    data_nascimento DATE,
+    profissao VARCHAR(20),
+);
