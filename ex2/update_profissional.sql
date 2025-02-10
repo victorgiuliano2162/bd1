d22adc56c53332fc8c053b96e27ade0cce5b5904
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-10 17:39:12 -0300

    modify profissional table

---
+++ ex2/update_profissional.sql
@@ -0,0 +1,11 @@
+UPDATE exercicios_bd2.profissional
+SET 
+    nome = CASE 
+        WHEN nome = 'Jacomorac Pacini' THEN 'Jacomorac Paccini'
+        WHEN nome = 'Van Borad' THEN 'Van Boraad'
+        ELSE nome 
+    END,
+    profissao = CASE 
+        WHEN profissao = 'Batedor de Massa' THEN 'Pedreiro'
+        ELSE profissao 
+    END;
